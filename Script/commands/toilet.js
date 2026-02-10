d7fc7bec36f172fb487601b37b4ab194c87ef642
fahimahmedraj40377-beep <fahimahmedraj40377@gmail.com>
2026-02-10 14:48:56 +0600

    Update toilet.js

--- Script/commands/toilet.js
+++ Script/commands/toilet.js
@@ -68,6 +68,6 @@ module.exports.run = async function ({ api, event, args, Currencies }) {
     );
 
   } catch (e) {
-    return api.sendMessage("API Error Call Boss SAHU", event.threadID, event.messageID);
+    return api.sendMessage("API Error Call Boss ᖴᴀʜɪᴍ ", event.threadID, event.messageID);
   }
 };
